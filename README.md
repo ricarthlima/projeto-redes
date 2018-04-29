d56abc6eea1e9867129aa1d9947beb7f480e4879
Ricarth Lima <rrsl@cin.ufpe.br>
2018-04-28 23:40:32 -0300

    Adição de POO
    O código que já existia foi adaptado para usar POO. A verificação de palíndromo foi retirada.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # projeto-redes
 ## Diário de Bordo
 
+### 28/04/2018
+O código que já existia foi adaptado para usar POO. A verificação de palíndromo foi retirada.
+
 ### 20/04/2018
 Foi feita a leitura do projeto e iniciado os primeiros testes que darão origem ao que será a questão 01. Por enquanto o arquivo possui duas funções, um Server UDP que escuta, e um Client UDP que envia. Ao receber algo do client, o server retorna aquilo é ou não um palíndromo. 
+
--- questao01.py
+++ questao01.py
@@ -1,47 +1,73 @@
 from socket import *
 
-def clientUDP(serverIP = "localhost", serverPort = 54321):
-    skt = socket(AF_INET,SOCK_DGRAM)
-    destination = (serverIP, serverPort)
+class ClientUDP:
+    def __init__(self,ip = "localhost", porta = 5000):
+        self.__ip = ip
+        self.__porta = porta
+        self.__socket = socket(AF_INET,SOCK_DGRAM)
+        print("CLIENT UDP: Cliente Iniciado.")
 
-    while True:
-        msg = input("> ")
-        if msg == "end":
-            break
-        else:
-            msg = bytes(msg,'utf-8')
-            skt.sendto(msg,destination)
+    def getIP(self):
+        return self.__ip
 
-            msgRec, addr = skt.recvfrom(3000)
-            print(msgRec,"\n")
+    def getPorta(self):
+        return self.__porta
 
-    skt.close()
-    return "Conexão Encerrada."
+    def getSocket(self):
+        return self.__socket
 
-def palindromo(string):
-    return string == string[::-1]
+    def setIP(self, ip):
+        self.__ip = ip
 
-def serverUDP(serverPort = 54321):
-    print("[Servidor UDP]\n")
-    skt = socket(AF_INET,SOCK_DGRAM)
-    skt.bind(('',serverPort))
-    while True:
-        print("Ouvindo na porta",serverPort)
-        msgRec, addr = skt.recvfrom(3000)
-        msgRec = msgRec.decode("utf-8")
-        print("Recebido <",msgRec,"> de <",addr,">")
-        if palindromo(msgRec):
-            print(True)
-            skt.sendto(bytes("True",'utf-8'),addr)
-        else:
-            print(False)
-            skt.sendto(bytes("False",'utf-8'),addr)
+    def setPorta(self, porta):
+        self.__porta = porta
+
+
+    def send(self,string):
+        msg = string.encode()
+        self.__socket.sendto(msg,(self.__ip, self.__porta))
+        print("CLIENT UDP: Mensagem enviada.")
+
+    def listen(self):
+        print("CLIENT UDP: Ouvindo na porta",str(self.__porta)+".")
+        msg, adr = self.__socket.recvfrom(1024)
+        print("CLIENT UDP: Mensagem recebida de",str(adr)+".")
+        msg = msg.decode()
+        return (msg, adr)
+
+    def close(self):
+        self.__socket.close()
+
+
+class ServerUDP:
+    def __init__(self,porta = 5000):
+        self.__porta = porta
+        self.__socket = socket(AF_INET,SOCK_DGRAM)
+        self.__socket.bind(('',self.__porta))
+        print("SERVER UDP: Server Iniciado.")
+
+    def getPorta(self):
+        return self.__porta
+
+    def setPorta(self,porta):
+        self.__porta = porta
         
-    
-if input() == "0":
-    serverUDP()
-else:
-    clientUDP()
-    
+    def rebind(self):
+        self.__socket.bind(('',self.__porta))
+        print("SERVER UDP: Server rebindado para porta ",str(self.__porta)+".")
+        
+    def listen(self):
+        print("SERVER UDP: Ouvindo na porta",str(self.__porta)+".")
+        msg, adr = self.__socket.recvfrom(1024)
+        print("SERVER UDP: Mensagem recebida de",str(adr)+".")
+        msg = msg.decode()
+        return (msg, adr)
+
+    def send(self,msg,ip):
+        msg = msg.encode()
+        self.__socket.sendto(msg,(ip,self.__porta))
+        print("CLIENT UDP: Mensagem enviada para",str(ip)+".")
+        
+ 
     
     
