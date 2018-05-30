51e83a0ad72800064bea52d56ecda36806e339b8
Ricarth Lima <rrsl@cin.ufpe.br>
2018-05-30 19:57:48 -0300

    comando post implementado
    com bugs

--- q2-client.py
+++ q2-client.py
@@ -1,16 +1,61 @@
 from socket import *
 
+def inverteBarra(string):
+    nova = ""
+    for letra in string:
+        if letra == '\\':
+            nova += "/"
+        else:
+            nova += letra
+    return nova
+
 def comandos(skt):
+    print()
     while True:
-        skt.send(input("cmd > ").encode())
-        print(skt.recv(5000))
+        msg = input("> ")
+        cmd = msg.split(" ")[0].upper()
+        carga = msg.split(" ") [1:]
+        
+        if cmd == "LS":
+            skt.send("LS".encode())
+            print(skt.recv(5000).decode())
+        elif cmd == "GET":
+            return
+        elif cmd == "POST":
+            file = open(carga[0],"br")   #Vamos ler o arquivo origem.
+            arq = file.read()
+            file.close()
+
+            nome = inverteBarra(carga[0]).split("/")[-1]
+
+            #Etapa 01 - Envio do comando e do nome do arquivo
+            skt.send(("POST "+nome).encode())
+            if "05NAMEOK" == (skt.recv(1024).decode()):
+                #Etapa 02 - Envio do diretorio em server do arquivo
+                skt.send(carga[1].encode())
+                if "05DIROK" == (skt.recv(1024).decode()):
+                    #Etapa 03 - Enviar o arquivo
+                    skt.send(arq)
+                    if "05ARQOK" == (skt.recv(1024).decode()):
+                        print("Arquivo transferido.")
+        elif cmd == "DELETE":
+            return
+        elif cmd == "SHARE":
+            return
+        elif cmd == "QUIT":
+            skt.send("QUIT".encode())
+            return
+        else:
+            print("Comando incorreto.")       
+                
 
 def auth(skt):
-    login = input("Insira o login:\n> ")
+    print()
+    login = input("login: ")
     skt.send(login.encode())
     msg = skt.recv(1024).decode()
     if msg == "02LOGINOK":
-        senha = input("Insira a senha:\n> ")
+        senha = input("senha: ")
         skt.send(senha.encode())
         msg = skt.recv(1024).decode()
         if msg == "03AUTHOK":
@@ -21,25 +66,36 @@ def auth(skt):
     return    
     
 def main():
-    ip = input("IP:\n> ")
-    if ip == "": ip = "localhost"
-    porta = input("Porta:\n> ")
-    if porta =="": porta = 6000
-    else: porta = int(porta)
+    #Splash Text
+    print("CloudRain - Client")
 
-    skt = socket(AF_INET,SOCK_STREAM)
-    skt.connect((ip,porta))
-    msg = skt.recv(1024).decode()
+    while True:
+        #Conexão
+        adr = input("Server Address: ")
+        if adr == "":
+            ip = "localhost"
+            porta = 6000
+        else:
+            ip = adr.split(":")[0]
+            try:
+                porta = int(adr.split(":")[1])
+            except:
+                print("Endereço do servidor incorreto.")
 
-    if msg == "01HELLO":
-        print("Conectado.")
-        auth(skt)
-        return
-    else:
-        print("Ocorreu um erro de conexão")
-        skt.close()
-        return
+        try:
+            skt = socket(AF_INET,SOCK_STREAM)
+            skt.connect((ip,porta))
+            msg = skt.recv(1024).decode()
 
+            if msg == "01HELLO":
+                print("Conectado.")
+                auth(skt)
+                break
+            else:
+                print("Ocorreu um erro de conexão")
+                skt.close()
+        except:
+            print("Ocorreu um erro na conexão. Tente novamente.")
 
 if __name__ == "__main__":
     main()
--- q2-server.py
+++ q2-server.py
@@ -43,10 +43,14 @@ class BancoDados:
     def cadastrar(self,login,senha):
         #Adiciona no banco de dados temporario
         try:
-            os.makedir("./arqs/"+login+"/")
+            os.makedirs("./arqs/"+login+"/")            
         except:
-            print("Não foi possível criar o diretório. Experimente abrir em modo administrador.")
+            print("Não foi possível criar o diretório.")
 
+        file = open("./lists/"+login+".txt","w")
+        file.write("Diretorio de "+login+".")
+        file.close()
+        
         self.__cadastrados[login] = (login,senha)
         self.salvar()
 
@@ -64,7 +68,7 @@ class BancoDados:
 def newConnectionChecker(skt,bd):    
     #Thread para adicionar 
     while True:        
-        skt.listen(5)
+        skt.listen(10)
         
         conexao, adr = skt.accept()         #Aceita a conexão
         _thread.start_new_thread(newConnectionChecker,(skt,bd))
@@ -102,40 +106,84 @@ def auth(login,senha,conexao,adr,bd):
 def chamaCMD(conexao,adr,login,bd):
     _thread.start_new_thread(newCommandChecker,(conexao,adr,login,bd)) #Chama a thread que vai ficar ouvindo essa conexao
 
+def appendDir(login,diretorio):
+    file = open("./lists/"+login+".txt","a")
+    file.write("\n"+diretorio)
+    file.close()
+    
 def newCommandChecker(conexao,adr,login,bd):
     #Thread para receber comandos
     while True:
-        try:
-            msg = conexao.recv(1024).decode()
+        msg = conexao.recv(1024).decode()
             
-            cmd = msg.split(" ")[0].upper()
-            carga = msg.split(" ") [1:]
+        cmd = msg.split(" ")[0].upper()
+        carga = msg.split(" ") [1:]
+         
+        if cmd == "LS":
+            print("CMD - Solicitação para visualizar diretório recebida",adr)
             
-            if cmd == "LS":
-                return
-            elif cmd == "GET":
-                return
-            elif cmd == "POST":
-                return
-            elif cmd == "DELETE":
-                return
-            elif cmd == "PUT":
-                return
-            elif cmd == "SHARE":
-                return
-            elif cmd == "QUIT":
-                conexao.send("END".encode())
-                conexao.close()
-                print("CMD - Conexão com",str(adr),"encerrada.")
-                break
-            else:
-                #Mensagem de erro
-                return
-        except:
+            try:
+                file = open("./lists/"+login+".txt","r")
+                texto = file.read()
+                file.close()
+            except:
+                texto = "Diretório vazio."
+            print(texto)  
+            conexao.send(texto.encode())
+        elif cmd == "GET":
+            return
+        elif cmd == "POST":
+            #Etapa 01 - Receber nome 
+            nome = carga[0]
+            conexao.send("05NAMEOK".encode())
+            print("CMD - Solicitação de transferencia de arquivo recebida.",adr)
+
+            #Etapa 02 - Receber o diretório e se preparar para receber arquivo
+            diretorio = conexao.recv(1024).decode()
+            conexao.send("05DIROK".encode())
+            print("CMD - Diretório recebido, aguardando arquivo.",adr)
+
+            '''
+            #Etapa 03 - Receber o arquivo 
+            file = open("./arqs/"+login+"/"+diretorio+"/"+nome,"bw")
+            while True:
+                dados = conexao.recv(1024)
+                if not dados:
+                    break
+                file.write(dados)
+            file.close()
+            appendDir(login,diretorio+nome)            
+            '''
+            
+            #Etapa 03 - Receber o arquivo            
+            arq = conexao.recv(4294967296)            
+            print("CMD - Arquivo recebido.",adr)           
+
+            #Etapa 04 - Gravar o arquivo
+            file = open("./arqs/"+login+"/"+diretorio+"/"+nome,"bw")
+            file.write(arq)
+            file.close()
+
+            appendDir(login,diretorio+nome)
+            
+            #Etapa 05 - Confirmação
+            conexao.send("05ARQOK".encode())
+            
+        elif cmd == "DELETE":
+            return
+        elif cmd == "PUT":
+            return
+        elif cmd == "SHARE":
+            return
+        elif cmd == "QUIT":
             conexao.send("END".encode())
             conexao.close()
-            print("CMD - Um erro ocorreu com a conexão",str(adr),"e ela teve de ser cancelada.")
-    
+            print("CMD - Conexão com",str(adr),"encerrada.")
+            break
+        else:
+            #Mensagem de erro
+            return
+        
 def main():
     bd = BancoDados()
     
